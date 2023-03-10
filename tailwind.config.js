8fde4843c50332867669bdad89f3060e00b5f017
Ivan Molto <ivanmolto@gmail.com>
2023-03-10 18:40:15 +0100

    chore: Customize font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -6,7 +7,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      sky: colors.sky,
+      green: colors.green,
+      purple: colors.purple,
+      green: colors.green,
+      pink: colors.pink,
+      yellow: colors.yellow,
+      wallcon: "#949be7",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
