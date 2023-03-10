0a91a14e34757f04f9a5793281aa7280a0c0d8e4
Ivan Molto <ivanmolto@gmail.com>
2023-03-10 11:06:24 +0100

    chore: Create Web3OnboardProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Layout from "../components/layout";
+
+import web3Onboard from "../web3-onboard";
+import { Web3OnboardProvider } from "@web3-onboard/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3OnboardProvider web3Onboard={web3Onboard}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Web3OnboardProvider>
+  );
 }
