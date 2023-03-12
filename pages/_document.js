c182a85e84cc9ae3b79c9c2a134890cd79722999
Ivan Molto <ivanmolto@gmail.com>
2023-03-12 16:24:02 +0100

    chore: Customize Document

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,66 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+  render() {
+    return (
+      <Html lang="en" class="h-full bg-black">
+        <Head>
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta content="#000000" name="theme-color" />
+          <meta content="#000000" name="msapplication-TileColor" />
+          <meta
+            content="/favicons/browserconfig.xml"
+            name="msapplication-config"
+          />
+          <link href="/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="favicons/site.webmanifest" rel="manifest" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            rel="preconnect"
+            href="https://ddz4ak4pa3d19.cloudfront.net"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            as="style"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+            media="print"
+            onLoad="this.media='all'"
+          />
+          <link
+            href="/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+        </Head>
+        <body class="h-full bg-black">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
